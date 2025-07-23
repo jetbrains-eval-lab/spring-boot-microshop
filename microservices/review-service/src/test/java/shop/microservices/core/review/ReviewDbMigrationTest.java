e1808a65fef312a06adef76c75058ba05bef0aa1
Daniil Tsarev <daniil.tsaryov@jetbrains.com>
2025-07-23 11:56:49 +0200

    #8 Review Service: Add Flyway for database migration
    - Add Flyway dependencies to manage DB schema migrations
    - Add 'V1__' migration script to create 'reviews' table with constraints
    - Add `ReviewDbMigrationTest` to validate Flyway migrations

---
+++ microservices/review-service/src/test/java/shop/microservices/core/review/ReviewDbMigrationTest.java
@@ -0,0 +1,45 @@
+package shop.microservices.core.review;
+
+import org.flywaydb.core.Flyway;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.simple.JdbcClient;
+
+import java.math.BigInteger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest(properties = {
+        "spring.flyway.clean-disabled: false"
+})
+public class ReviewDbMigrationTest extends MySqlTestBase {
+
+    @Autowired
+    private Flyway flyway;
+
+    @Autowired
+    private JdbcClient jdbcClient;
+
+    @Test
+    void testMigration() {
+        flyway.clean();
+        flyway.migrate();
+
+        BigInteger dbMajorVersion = flyway.info().current().getVersion().getMajor();
+
+        assertThat(dbMajorVersion)
+                .isEqualTo(1);
+
+        try {
+            //noinspection SqlDialectInspection
+            var rows = jdbcClient.sql("select review_id from reviews")
+                    .query()
+                    .listOfRows();
+
+            assertThat(rows.size()).isEqualTo(0);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
